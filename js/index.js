079adcf6f1e29fd6eda0a31123a27742313e554d
hadi <hadi.yousefi@students.beaminstitute.org>
2024-01-31 15:11:08 +0100

    contactform1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 import { API_Key_NASA } from "./credentials.js";
 
+
 const nasaCall = () => {
   const randomNumber = Math.floor(Math.random() * 30);
 
@@ -9,22 +10,41 @@ const nasaCall = () => {
     .then((response) => response.json())
     // .then((json) => console.log(json))
     .then((data) => {
-      const paragraph = document.createElement("p");
+     let allimage=document.querySelectorAll("img");//
+
+      if (allimage.length<=1){//
+        const paragraph = document.createElement("p");
+        paragraph.id = "myparagraph";//
       paragraph.textContent = data.explanation;
       paragraph.style.fontSize = "20px";
       const image = document.createElement("img");
+      
+      image.id = "myimage";//
       image.src = data.hdurl;
       image.style.width = "500px";
       image.style.height = "500px";
       image.style.margin = "auto";
       document.body.appendChild(paragraph);
       document.body.appendChild(image);
+      }
+      else//
+      {
+     const paragraph = document.querySelector("#myparagraph")
+      paragraph.textContent = data.explanation;
+    
+     const image = document.querySelector("#myimage")
+    
+      image.src = data.hdurl;
+     
+      }
     });
 };
 
 
 const gtn = document.querySelector('#gtn');
+
 gtn.addEventListener('click', (event) => {
   event.preventDefault()
   nasaCall()
+ 
 })
